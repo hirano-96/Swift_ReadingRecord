10d25256ee19dc86c318023146dbdba12964e3f6
fuji <fujiyoshiyuki@gmail.com>
2023-02-25 01:10:01 +0900

    ログイン画面修正

--- ReadingRecord/ReadingRecord/LoginView.swift
+++ ReadingRecord/ReadingRecord/LoginView.swift
@@ -16,35 +16,55 @@ struct LoginView: View {
             
             VStack(alignment: .center){
                 Text("Reading Record").font(.system(size: 48,weight: .heavy))
-                    .foregroundColor(Color.accentColor)
+                    .foregroundColor(Color.primary)
                 
                 VStack(spacing: 24) {
                     TextField("Name", text: $inputName)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(maxWidth: 280)
+                        .overlay(RoundedRectangle(cornerSize: CGSize(width: 8.0, height: 8.0))
+                            .stroke(Color.primary))
                     
                     SecureField("Password", text: $inputPassword)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .frame(maxWidth: 280)
+                        .overlay(RoundedRectangle(cornerSize: CGSize(width: 8.0, height: 8.0))
+                            .stroke(Color.primary))
                 }
                 .frame(height: 100)
                 
-                Button(action: {
-                    print("ログイン処理")
-                },
-                label: {
-                    Text("ログイン")
-                        .fontWeight(.medium)
-                        .frame(minWidth: 160)
-                        .foregroundColor(Color.white)
-                        .padding(12)
-                        .background(Color.accentColor)
-                        .cornerRadius(8)
-                })
+                HStack{
+                    Button(action: {
+                        print("ユーザー作成")
+                    },
+                    label: {
+                        Text("Cerate User")
+                            .fontWeight(.medium)
+                            .frame(minWidth: 160)
+                            .foregroundColor(Color.secondary)
+                            .padding(12)
+                            .cornerRadius(8)
+                    })
+                    
+                    Button(action: {
+                        print("ログイン処理")
+                    },
+                    label: {
+                        Text("Login")
+                            .fontWeight(.medium)
+                            .frame(minWidth: 160)
+                            .foregroundColor(Color.primary)
+                            .padding(12)
+                            .background(Color.gray)
+                            .cornerRadius(8)
+                    })
+                    
+                }
                 Spacer()
             }
             .frame(height: 400)
         }
+
     }
 }
 
